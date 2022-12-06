10bcaafb94aa916ad7bc1e734c21c19911506ce0
Burxon <anonymous.nb2005@gmail.com>
2022-12-06 10:53:15 -0500

    first-commit

---
+++ client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	arifmeticService "github.com/burxondv/grpc_start/genproto/arifmetic_service"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	client := arifmeticService.NewArifmeticServiceClient(conn)
+
+	resp, err := client.Add(context.Background(), &arifmeticService.Request{A: 1231, B: 345})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Result: ", resp.Result)
+
+	resp, err = client.Divide(context.Background(), &arifmeticService.Request{A: 1231, B: 345})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Result: ", resp)
+}
